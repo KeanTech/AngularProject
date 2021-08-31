f51d68643c30c110cf6371a663fdd46fcf965771
Kean <50844146+Mojiez@users.noreply.github.com>
2021-08-31 11:44:51 +0200

    Removed autogenerated code, and added new controller

---
+++ ZBC_TempetureAPI/ZBC_TempetureAPI/Controllers/TemperatureController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZBC_TempetureAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperatureController : Controller
+    {
+        [HttpPost]
+        public void Post(string input)
+        {
+            
+        }
+
+
+    }
+}
