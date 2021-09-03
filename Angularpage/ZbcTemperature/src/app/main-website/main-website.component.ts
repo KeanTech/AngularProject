389e59081df62c5963f9213ed3e2fde0c120c080
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2021-09-03 12:55:17 +0200

    All

---
+++ Angularpage/ZbcTemperature/src/app/main-website/main-website.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-main-website',
+  templateUrl: './main-website.component.html',
+  styleUrls: ['./main-website.component.css']
+})
+export class MainWebsiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
