7963314a6567d42b3adef9526f8f42459a3f09ab
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2021-09-09 09:28:29 +0200

    cc

--- Angularpage/ZbcTemperature/src/app/main-website/main-website.component.ts
+++ Angularpage/ZbcTemperature/src/app/main-website/main-website.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { RoomsandtempsService } from '../roomsandtemps.service';
+import { Observable } from 'rxjs';
+import { Data } from '@angular/router';
+import { AppComponent } from '../app.component';
+import { NgxJsonViewModule } from 'ng-json-view';
+
+export interface Room {
+  Name: string;
+  Temperature: number;
+}
+/*
+const RoomData: Room[] = [
+  {Name: 'H.01', Temperature: 23.1},
+  {Name: 'H.02', Temperature: 24.1},
+];
+*/
+
+
+var appComp:AppComponent;
+let roomData: string[];
+
 
 @Component({
   selector: 'app-main-website',
   templateUrl: './main-website.component.html',
   styleUrls: ['./main-website.component.css']
 })
+
+
 export class MainWebsiteComponent implements OnInit {
 
-  constructor() { }
+  roomData = appComp.GetData();
+  data = roomData;
+  displayedColumns = ['Name', 'Temperature'];
 
   ngOnInit(): void {
   }
-
 }
