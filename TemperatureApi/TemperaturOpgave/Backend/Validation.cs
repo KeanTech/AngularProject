cbe72ac542bf191009e1ef6ac0adc2e57d667a7e
MoJeez <kenethanders@gmail.com>
2021-09-29 10:50:54 +0200

    Added to classes Validation for users and dccommunication for future development

---
+++ TemperatureApi/TemperaturOpgave/Backend/Validation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemperaturOpgave.Backend
+{
+    public class Validation
+    {
+    }
+}
